9d0b31e5abb5bddb8a394f5bba8b28758fafec6b
Alan Tsai <alanalan216@gmail.com>
2016-10-22 17:35:16 -0700

    generator

--- es6.js
+++ es6.js
@@ -172,6 +172,28 @@ const gen = numbers();
 gen.next();
 gen.next();
 
+function* shopping() {
+  // stuff on the sidewalk
+  
+  // walking down the sidewalk
+  
+  // go into the store with cash
+  const stuffFromStore = yield 'cash';  
+  
+  // walking back home
+  return stuffFromStore;
+}
+// stuff in the store
+const gen = shopping();
+gen.next(); // leaving our home
+// walked into the store
+// walking up and down the aisles..
+// purchase our stuff
+
+
+gen.next('groceries'); // leaving the store with groceries
+
+
 
 
 
