8a74fa24e9995b4ec824861f58de8a3d5540343f
Alan Tsai <alanalan216@gmail.com>
2016-10-23 01:09:51 -0700

    generator

--- es6.js
+++ es6.js
@@ -200,7 +200,26 @@ gen.next('clean clothes');
 
 
 
+const engineeringTeam = {
+  size: 3,
+  department: 'Engineering',
+  lead: 'Jill',
+  manager: 'Alex',
+  engineer: 'Dave'
+}
 
+function* TeamIterator(team) {
+  yield team.lead;
+  yield team.manager;
+  yield team.engineer;
+}
+
+const names = [];
+for(let name of TeamIterator(engineeringTeam)) {
+  names.push(name);
+}
+
+names;
 
 
 
