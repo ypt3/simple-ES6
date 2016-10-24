801c53a9c01a791d13309b74e56300087d44ee53
Alan Tsai <alanalan216@gmail.com>
2016-10-24 14:52:58 -0700

    promise

--- es6.js
+++ es6.js
@@ -314,7 +314,11 @@ promise
   .then(() => {console.log('i was also ran!!!')})
   .catch(() => {console.log('uh oh!')});
 
+url = "https://jsonplaceholder.typicode.com/posts";
 
+fetch(url)
+  .then(response => response.json())
+  .then(data => console.log(data));
 
 
 
