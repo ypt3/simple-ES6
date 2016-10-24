733141cf2424ee56b6aa4a816040ad1cfef65489
Alan Tsai <alanalan216@gmail.com>
2016-10-24 14:44:51 -0700

    promise

--- es6.js
+++ es6.js
@@ -300,6 +300,21 @@ for (let value of tree) {
 
 values;
 
+promise = new Promise((resolve, reject) => {
+  var request = new XHTMLRequest()
+  // make request
+  request.onload = () => {
+    resolve();
+  };
+});
+
+
+promise
+  .then(() => {console.log('finally finished!')})
+  .then(() => {console.log('i was also ran!!!')})
+  .catch(() => {console.log('uh oh!')});
+
+
 
 
 
